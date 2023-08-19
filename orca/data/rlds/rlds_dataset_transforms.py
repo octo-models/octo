f91e19acb8313518c05e7f584ba5318495240cf7
Karl Pertsch <pertsch@usc.edu>
2023-08-18 18:06:35 -0700

    implement r2d2 transform

--- orca/data/rlds/rlds_dataset_transforms.py
+++ orca/data/rlds/rlds_dataset_transforms.py
@@ -20,6 +20,21 @@ def stanford_kuka_multimodal_dataset_transform(
     return trajectory
 
 
+def r2_d2_dataset_transform(
+    trajectory: Dict[str, Any]
+) -> Dict[str, Any]:
+    # every input feature is batched, ie has leading batch dimension
+    trajectory["action"] = tf.concat(
+        (
+            trajectory["action_dict"]["cartesian_position"],
+            trajectory["action_dict"]["gripper_position"],
+        ),
+        axis=-1,
+    )
+    return trajectory
+
+
 RLDS_TRAJECTORY_MAP_TRANSFORMS = {
     "stanford_kuka_multimodal_dataset": stanford_kuka_multimodal_dataset_transform,
+    "r2_d2_dataset": r2_d2_dataset_transform,
 }
