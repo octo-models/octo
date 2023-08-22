5235be48d3ba9251f778c9201232474b2972700f
HomerW <hrwalke@gmail.com>
2023-08-22 15:52:06 -0700

    update bridge eval scripts

--- experiments/homer/scripts/eval.sh
+++ experiments/homer/scripts/eval.sh
@@ -1,21 +1,21 @@
 MODELS=(
-    "orca_bridgedata/bc_20230530_082159"
+    "orca/bridge_goal_20230821_042544"
 )
 
 CKPTS=(
-    "140000"
+    "100000"
 )
 
-VIDEO_DIR="5-30"
+VIDEO_DIR="8-22"
 
-CMD="python experiments/homer/eval_policy.py \
+CMD="python experiments/homer/bridge/eval.py \
     --num_timesteps 100 \
     --video_save_path ../../trainingdata/homer/videos/$VIDEO_DIR \
     $(for i in "${!MODELS[@]}"; do echo "--checkpoint_path gs://rail-tpus-homer/log/${MODELS[$i]}/checkpoint_${CKPTS[$i]} "; done) \
     $(for i in "${!MODELS[@]}"; do echo "--wandb_run_name widowx-gcrl/${MODELS[$i]} "; done) \
-    --blocking \
+    --im_size 256 \
 "
 
 echo $CMD
 
-$CMD --goal_eep "0.3 0.0 0.1" --initial_eep "0.3 0.0 0.1"
+$CMD --goal_eep "0.3 0.0 0.15" --initial_eep "0.3 0.0 0.15" --obs_horizon 2 --act_pred_horizon 1
