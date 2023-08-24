f0b791cb1a2d6e5bcd0e6ec67105a51ea4ab38be
Kevin Black <kevin3.black@gmail.com>
2023-08-24 15:44:24 -0700

    Initial dlimp dataloader refactor

---
+++ orca/data/utils/bc_goal_relabeling.py
@@ -0,0 +1,28 @@
+"""
+Contains simple goal relabeling logic for BC use-cases where rewards and next_observations are not required.
+"""
+
+import tensorflow as tf
+
+
+def uniform(traj):
+    """
+    Relabels with a true uniform distribution over future states.
+    """
+    traj_len = tf.shape(tf.nest.flatten(traj["observations"])[0])[0]
+
+    # select a random future index for each transition i in the range [i + 1, traj_len)
+    rand = tf.random.uniform([traj_len])
+    low = tf.cast(tf.range(traj_len) + 1, tf.float32)
+    high = tf.cast(traj_len, tf.float32)
+    goal_idxs = tf.cast(rand * (high - low) + low, tf.int32)
+
+    # sometimes there are floating-point errors that cause an out-of-bounds
+    goal_idxs = tf.minimum(goal_idxs, traj_len - 1)
+
+    traj["goals"] = tf.nest.map_structure(
+        lambda x: tf.gather(x, goal_idxs),
+        traj["observations"],
+    )
+
+    return traj
