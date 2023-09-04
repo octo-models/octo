53c9d567ac5272088436d74464181d5b64e994c2
HomerW <hrwalke@gmail.com>
2023-09-04 01:39:00 +0000

    use right dataset

--- experiments/homer/scripts/train_bridge.sh
+++ experiments/homer/scripts/train_bridge.sh
@@ -8,12 +8,10 @@ TPU3="export TPU_VISIBLE_DEVICES=3 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 TPU_HOST_BOUN
 TPU01="export TPU_VISIBLE_DEVICES=0,1 TPU_CHIPS_PER_HOST_BOUNDS=1,2,1 TPU_HOST_BOUNDS=1,1,1 TPU_MESH_CONTROLLER_ADDRESS=localhost:8476 TPU_MESH_CONTROLLER_PORT=8476"
 TPU23="export TPU_VISIBLE_DEVICES=2,3 TPU_CHIPS_PER_HOST_BOUNDS=1,2,1 TPU_HOST_BOUNDS=1,1,1 TPU_MESH_CONTROLLER_ADDRESS=localhost:8478 TPU_MESH_CONTROLLER_PORT=8478"
 
-export CUDA_VISIBLE_DEVICES=1
-
-NAME="test"
+NAME="bridge_goal_relabel_actions_obs_hist_1_bridge_loader"
 
 CMD="python experiments/homer/train_bridge.py \
     --config experiments/homer/configs/train_config.py:transformer_bc_bridge \
     --name $NAME"
 
-$CMD --debug
+$CMD
