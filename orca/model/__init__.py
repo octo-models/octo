ca50177cedfb0d53df4a9c429e2c773f12018695
Karl Pertsch <pertsch@usc.edu>
2023-09-24 17:18:05 -0700

    move all tokenizer creation into ORCAPolicy

--- orca/model/__init__.py
+++ orca/model/__init__.py
@@ -1,13 +1,9 @@
 import logging
 
-from orca.model.components.tokenizers import TOKENIZERS
-
 from .orca_policy import ORCAPolicy
 
 
 def create_model_def(
-    observation_tokenizers,
-    task_tokenizers,
     action_dim,
     window_size,
     policy_kwargs,
@@ -15,15 +11,8 @@ def create_model_def(
 ):
     if len(kwargs) > 0:
         logging.warn(f"Extra kwargs passed into create_model_def: {kwargs}")
-    observation_tokenizer_defs = tuple(
-        TOKENIZERS[tokenizer](**kwargs) for tokenizer, kwargs in observation_tokenizers
-    )
-    task_tokenizer_defs = tuple(
-        TOKENIZERS[tokenizer](**kwargs) for tokenizer, kwargs in task_tokenizers
-    )
+
     model_def = ORCAPolicy(
-        observation_tokenizers=observation_tokenizer_defs,
-        task_tokenizers=task_tokenizer_defs,
         action_dim=action_dim,
         window_size=window_size,
         **policy_kwargs,
