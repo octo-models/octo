19b40d900be2dcc9a96faa8efb8bb354a8d71ad2
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2023-10-18 23:20:41 +0000

    Making token type an enum

--- orca/model/components/__init__.py
+++ orca/model/components/__init__.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import auto, Enum
 
 from orca.model.components.resnet_v1 import resnetv1_configs
 
@@ -6,10 +7,16 @@ encoders = dict()
 encoders.update(resnetv1_configs)
 
 
+class TokenType(Enum):
+    TASK = auto()
+    OBS = auto()
+    ACTION = auto()
+
+
 @dataclass
 class TokenMetadata:
     """Useful metadata for computing attention masks"""
 
-    name: str  # either 'task', 'obs', or 'action'
+    kind: TokenType  # either 'task', 'obs', or 'action'
     timestep: int  # What timestep the token belongs to
     extra_metadata: dict = None  # Any extra information useful for attention mask
