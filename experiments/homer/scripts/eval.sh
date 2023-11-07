eaa097b54ce95e350758aba2ce368fee7fb66297
HomerW <hrwalke@gmail.com>
2023-11-06 19:22:41 -0800

    update pretrained utils and eval script

--- experiments/homer/scripts/eval.sh
+++ experiments/homer/scripts/eval.sh
@@ -15,7 +15,8 @@ TEMPERATURE="0.1"
 CMD="python experiments/homer/bridge/eval.py \
     --num_timesteps $TIMESTEPS \
     --video_save_path /mount/harddrive/homer/videos/$VIDEO_DIR \
-    $(for i in "${!NAMES[@]}"; do echo "--checkpoint_weights_path /mount/harddrive/homer/checkpoints/${NAMES[$i]}/checkpoint_${STEPS[$i]} "; done) \
+    $(for i in "${!NAMES[@]}"; do echo "--checkpoint_weights_path /mount/harddrive/homer/checkpoints/${NAMES[$i]} "; done) \
+    $(for i in "${!NAMES[@]}"; do echo "--checkpoint_step /mount/harddrive/homer/checkpoints/${STEPS[$i]} "; done) \
     $(for i in "${!NAMES[@]}"; do echo "--checkpoint_config_path /mount/harddrive/homer/checkpoints/${NAMES[$i]}/config.json "; done) \
     $(for i in "${!NAMES[@]}"; do echo "--checkpoint_metadata_path /mount/harddrive/homer/checkpoints/${NAMES[$i]}/action_proprio_metadata_bridge_dataset.json "; done) \
     $(for i in "${!NAMES[@]}"; do echo "--checkpoint_example_batch_path /mount/harddrive/homer/checkpoints/${NAMES[$i]}/example_batch.msgpack "; done) \
