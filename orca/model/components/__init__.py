85ac55d194e194e4e005011a11acc4f5d5854c9d
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2023-11-13 03:47:10 +0000

    Added simple encoders for pure transformer architectures

--- orca/model/components/__init__.py
+++ orca/model/components/__init__.py
@@ -1,4 +1,6 @@
 from orca.model.components.resnet_v1 import resnetv1_configs
+from orca.model.components.simple_encoders import simple_encoder_configs
 
 encoders = dict()
 encoders.update(resnetv1_configs)
+encoders.update(simple_encoder_configs)
