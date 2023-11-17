6ad96a036ae2948f20cfc7d0de93bf70f7e63632
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2023-11-17 19:05:42 +0000

    Adding chuhnking

--- experiments/dibya/oxe_config.py
+++ experiments/dibya/oxe_config.py
@@ -44,6 +44,7 @@ def get_config(
 
     base_data_config = dict(
         window_size=1,
+        additional_action_window_size=0,
         image_augment_kwargs=dict(
             random_resized_crop=dict(scale=[0.8, 1.0], ratio=[0.9, 1.1]),
             random_brightness=[0.2],
