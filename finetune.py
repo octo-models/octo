31b2ee648ea5cc8dad0699944c297d4fecdd5858
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2023-11-30 02:20:57 +0000

    Adding better param norms

--- finetune.py
+++ finetune.py
@@ -178,7 +178,7 @@ def main(_):
     rng = jax.random.PRNGKey(FLAGS.config.seed)
 
     params = model.params.unfreeze()
-    tx, lr_callable = create_optimizer(
+    tx, lr_callable, param_norm_callable = create_optimizer(
         params,
         FLAGS.config.optimizer.to_dict(),
     )
@@ -294,14 +294,13 @@ def main(_):
         )
         # Gradient Metrics (TODO: Does the finetuner need these?) ###
         grad_norm = optax.global_norm(grads)
-        param_norm = optax.global_norm(state.params)
         updates, _ = state.tx.update(grads, state.opt_state, state.params)
         update_norm = optax.global_norm(updates)
         info.update(
             {
                 "grad_norm": grad_norm,
-                "param_norm": param_norm,
                 "update_norm": update_norm,
+                "param_norm": param_norm_callable(state.params),
                 "learning_rate": lr_callable(state.step),
             }
         )
