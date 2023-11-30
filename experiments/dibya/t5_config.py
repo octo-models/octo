d55f09fdc475df0795806b7bb30a43f71f865832
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2023-11-30 02:40:39 +0000

    Adding support for freezing

--- experiments/dibya/t5_config.py
+++ experiments/dibya/t5_config.py
@@ -1,39 +1,41 @@
-from copy import deepcopy
+from config import get_config as get_base_config
+from config import update_config, wrap
 
-from config import update_config
-from ml_collections import ConfigDict
-from ml_collections.config_dict import placeholder
-from experiments.dibya.oxe_config import get_config as get_base_config
 
-def get_config(
-    transformer_size,
-):
-    base_config = get_base_config(transformer_size)
+def get_config(config_string=None):
+    base_config = get_base_config(config_string)
 
-    config = update_config(base_config,
+    config = update_config(
+        base_config,
+        optimizer=dict(
+            frozen_keys=("*hf_model*",),
+        ),
         model={
             "observation_tokenizers": [
-                    (
-                        "image_tokenizer",
-                        {
-                            "num_tokens": 64,
-                            "encoder": "small-stem-16",
-                            "encoder_kwargs": dict(),
-                            "task_stack_keys": [
-                                "image_.*"
-                            ],  # by default, early fuse goal images into visual encoder
-                        },
-                    ),
-                ],
-                "task_tokenizers": [
-                    ("language_tokenizer", {"encoder": "t5-base", "finetune_encoder": False}),
-                ],
+                (
+                    "image_tokenizer",
+                    {
+                        "num_tokens": 256,
+                        "encoder": "small-stem-16",
+                        "encoder_kwargs": dict(),
+                        "task_stack_keys": [
+                            "image_.*"
+                        ],  # by default, early fuse goal images into visual encoder
+                    },
+                ),
+            ],
+            "task_tokenizers": [
+                (
+                    "language_tokenizer",
+                    {"encoder": "t5-base", "finetune_encoder": False},
+                ),
+            ],
         },
         text_processor="hf_tokenizer",
         text_processor_kwargs=dict(
             tokenizer_name="t5-base",
             encode_with_model=False,
-            tokenizer_kwargs= {
+            tokenizer_kwargs={
                 "max_length": 16,
                 "padding": "max_length",
                 "truncation": True,
