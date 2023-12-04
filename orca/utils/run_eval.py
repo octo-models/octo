aa71cfd00115d2358bb1397334ad65893278d768
HomerW <hrwalke@gmail.com>
2023-12-04 13:16:24 -0800

    fix undefined var

--- orca/utils/run_eval.py
+++ orca/utils/run_eval.py
@@ -5,9 +5,9 @@ from functools import partial
 import json
 import os
 import time
-from typing import Callable, Optional
+from typing import Callable
 
-from absl import app, flags, logging
+from absl import flags, logging
 import click
 import cv2
 import flax
@@ -18,7 +18,6 @@ import jax.numpy as jnp
 import numpy as np
 import tensorflow as tf
 
-from orca.sim.widowx_sim_env import WidowXSimEnv
 from orca.utils.eval_utils import (
     download_checkpoint_from_gcs,
     load_jaxrlm_checkpoint,
