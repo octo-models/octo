f0ee5da3965e55200a671eeadcb7a653b7ab6723
Kevin Black <kevin3.black@gmail.com>
2023-12-07 14:11:50 -0800

    Remove jax dependency from text_processing.py

--- orca/data/utils/text_processing.py
+++ orca/data/utils/text_processing.py
@@ -1,7 +1,5 @@
 from typing import Optional, Sequence
 
-from flax.core import FrozenDict
-import jax.numpy as jnp
 import numpy as np
 import tensorflow as tf
 
@@ -81,10 +79,10 @@ class CLIPTextProcessor(TextProcessor):
             text=strings,
             **self.kwargs,
         )
-        inputs["position_ids"] = jnp.expand_dims(
-            jnp.arange(inputs["input_ids"].shape[1]), axis=0
+        inputs["position_ids"] = np.expand_dims(
+            np.arange(inputs["input_ids"].shape[1]), axis=0
         ).repeat(inputs["input_ids"].shape[0], axis=0)
-        return FrozenDict(inputs)
+        return inputs
 
 
 text_processors = {
