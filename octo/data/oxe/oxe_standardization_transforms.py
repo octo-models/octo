4813bbe3a1d9b5c81bc6c249521a6bc0b094b444
Kevin Black <kevin3.black@gmail.com>
2023-12-14 00:41:33 -0800

    Update example to use actual OXE path, clarify bridge

--- octo/data/oxe/oxe_standardization_transforms.py
+++ octo/data/oxe/oxe_standardization_transforms.py
@@ -25,6 +25,8 @@ from octo.data.utils.data_utils import (
 
 
 def bridge_dataset_transform(trajectory: Dict[str, Any]) -> Dict[str, Any]:
+    # NOTE: this is not actually the official OXE copy of bridge, it is our own more up-to-date copy that you
+    # can find at https://rail.eecs.berkeley.edu/datasets/bridge_release/data/tfds/
     trajectory["action"] = tf.concat(
         [
             trajectory["action"][:, :6],
