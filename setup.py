721b068e59d77fa733127d6c02df738f7e101983
Sebastian Peralta <peraltas@seas.upenn.edu>
2024-02-03 22:27:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup
 
-setup(name="octo", packages=["octo"])
+setup(
+    name="octo",
+    version="0.0.1",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+      "jax>=0.4.20",
+      "jaxlib>=0.4.20",
+      "dlimp @ git+https://github.com/kvablack/dlimp@d08da3852c149548aaa8551186d619d87375df08",
+      "tensorflow_datasets <= 4.9.3",  # Required for gsutil to work as intended. See https://github.com/tensorflow/datasets/issues/5203.
+    ],
+)
